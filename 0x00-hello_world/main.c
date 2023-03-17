413f5f76822166c71de11a1ddf4274d8a0f991f6
vekwu <ekwuvictory@gmail.com>
2023-03-17 02:35:07 +0000

    updated

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
 /**
- ** main - Entry point
+ *  * main - Entry point
+ *  *
+ *  * Return: Always 0 (Success)
  *
- *  Return: Always 0 (success)
- */
- int main (void)
- {
-	return (0);
- }
+ **/
+int main(void)
 
+{
+	return (0);
+
+}
